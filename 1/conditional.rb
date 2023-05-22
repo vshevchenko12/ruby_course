6eb8ca39ae0b4af61181b2b4640d9861f5b2133a
Vitalii Shevchenko <vshevchenko12@gmail.com>
2023-05-21 23:45:35 -0300

    covering conditional

---
+++ 1/conditional.rb
@@ -0,0 +1,17 @@
+if 2 < 1
+  puts "yes"
+else
+  puts "no"
+end
+
+puts "yes" unless 2 < 1
+
+name = "test"
+
+if name == "test2"
+  puts "name is " + name
+elsif name == "test3"
+  puts "name is " + name
+else 
+  puts "name is " + name
+end
