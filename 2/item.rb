7aa065be686970734fb2ae61cb66b567c4963294
Vitalii Shevchenko <vshevchenko12@gmail.com>
2023-07-11 23:55:51 -0300

    Added options to initialize using the hash

--- 2/item.rb
+++ 2/item.rb
@@ -1,8 +1,8 @@
  # frozen_string_literal: true
 
 class Item
-  def initialize
-    @price = 30
+  def initialize(options)
+    @price = options[:price]
   end
 
   #  GETTER AND SETTER version 1
@@ -27,11 +27,11 @@ class Item
   attr_accessor :price
 end
 
-item1 = Item.new
-item2 = Item.new
+item1 = Item.new({ price: 100 })
+item2 = Item.new({ price: 200 })
 
 # item1.price = 10
-item2.price = 20
+# item2.price = 20
 
 p item1.price
 p item2.price
