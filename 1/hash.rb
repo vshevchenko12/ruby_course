b87a89cdf05b5416586b13fcba93c98b32e41986
Vitalii Shevchenko <vshevchenko12@gmail.com>
2023-07-11 23:49:08 -0300

    covering the hash

---
+++ 1/hash.rb
@@ -0,0 +1,11 @@
+# New version of the hash
+john = { :name => "John", :age => 21}
+
+# Old version of the hash
+mike = { name: "Mike", age: 23}
+
+p john[:name]
+p john[:age]
+puts '--------------------------------'
+p mike[:name]
+p mike[:age]
