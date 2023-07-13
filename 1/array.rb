8ba134d66fe57c8a1bcf93e409936b4beded1eb7
Vitalii Shevchenko <vshevchenko12@gmail.com>
2023-07-12 23:57:09 -0300

    Covering array's methods

---
+++ 1/array.rb
@@ -0,0 +1,19 @@
+my_arr = %w(John Michael Nick)
+puts my_arr
+p my_arr
+
+p my_arr.class
+my_arr.push('Alice')
+p my_arr
+
+my_arr.pop
+p my_arr
+
+my_arr.shift
+p my_arr
+
+puts '--------------------------------'
+
+my_string_array = %w[apple banana cherry]
+puts my_string_array
+p my_string_array
