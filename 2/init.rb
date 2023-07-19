c1e6a2db57dd76928f8c7b8f43cd4a6bb1fbc93a
Vitalii Shevchenko <vshevchenko12@gmail.com>
2023-07-18 23:48:48 -0300

    procced covering the blocks

--- 2/init.rb
+++ 2/init.rb
@@ -1,10 +1,16 @@
 require_relative 'cart'
 require_relative 'item'
 
-cart = Cart.new
-cart.add_item(Item.new({price: 200}))
-cart.add_item(Item.new)
-cart.validate
-cart.remove_item
+# cart = Cart.new
+# cart.add_item(Item.new({price: 200}))
+# cart.add_item(Item.new)
+# cart.validate
+# cart.remove_item
 
-p cart
+# p cart
+
+item = Item.new({price: 25, weight: 120, name: 'Car'})
+item1 = Item.new({price: 25, weight: 120, name: 'Car'})
+
+item.info { |value| puts value }
+item1.info
--- 2/item.rb
+++ 2/item.rb
@@ -20,11 +20,22 @@ class Item
   # attr_writer :price
 
   # GETTER AND SETTER version 3
-  attr_accessor :price, :weight
+  attr_accessor :price, :weight, :name
 
   def initialize(options = {})
     @price = options[:price]
     @weight = options[:weight]
+    @name = options[:name]
+  end
+
+  def info
+    if block_given?
+      yield price
+      yield weight
+      yield name
+    else
+      puts 'No price specified'
+    end
   end
 end
 
